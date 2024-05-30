efe69b54c0b24b027f89091eff0b0bc5f1913e32
quinteirosm <quinteiros.m@gmail.com>
2024-05-31 01:46:56 +0200

    repair

--- prisma/migrations/20240530232255_/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX `Chips_userId_fkey` ON `Chips`;
-
--- DropIndex
-DROP INDEX `Documents_chipId_fkey` ON `Documents`;
-
--- DropIndex
-DROP INDEX `Documents_userId_fkey` ON `Documents`;
